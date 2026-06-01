cdac50b2fc965ba4b42c51140998b222c957a6df
NguyenAnh-lab <tacoroccohabbo@gmail.com>
2026-06-01 14:04:22 +0700

    final

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-const envelope = document.getElementById("envelope-container");
-const letter = document.getElementById("letter-container");
-
-envelope.addEventListener("click", () => {
-    envelope.style.display = "none";
-    letter.style.display = "flex";
-
-    setTimeout(() => {
-        document.querySelector(".letter-window").classList.add("open");
-    }, 50);
+requestAnimationFrame(() => {
+    document.getElementById("letter-container").classList.add("open");
 });
